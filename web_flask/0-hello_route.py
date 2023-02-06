2633978d883727f6f9a2dd1a8d234ade2fd4e33a
Frederick Ankomah <ankomahfrederickyahoo@gmail.com>
2023-02-06 22:35:33 +0000

    Write a script that starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Web application using Flask Framework """
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
@@ -8,7 +10,6 @@ app = Flask(__name__)
 def hello_hbnb():
     """ Funcion that displays a text """
     return 'HELLO HBNB!'
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
